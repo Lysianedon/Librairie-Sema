c31e578edf15cf6fc83e519086843ae9d1817a23
lysiandon <don.lysiane@gmail.com>
2022-09-02 01:40:46 +0200

    started working on bookview

--- frontend/src/views/BookView.vue
+++ frontend/src/views/BookView.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-      <h1>{{title}}</h1>
+   <div class="personalised-suggestion">
+         <img
+            class="img"
+            :src="book.image"
+            :alt="`page de couverture du livre : \'${book.title} \' `"
+            srcset="">
+          <div class="book-infos">
+            <h3>{{book.title}}</h3>
+            <h4>De: {{book.author}}</h4>
+            <h4>Genre: {{book.genre}}</h4>
+            <h4>Pays: {{book.country}}</h4>
+            <h4 class="synopsis-title">Synopsis:</h4>
+            <p> {{ book.synopsis}}</p>
+
+            <div class="btns">
+              <button class="btn-option" @click="addToLibrary(book._id)">Ajouter à ma bibliothèque</button>
+              <button class="btn-option" @click="addToFavorites(book._id)">Ajouter à mes favoris</button>
+            </div>
+          </div>
     </div>
 </template>
 
@@ -10,7 +27,13 @@ export default {
   name: 'BookView',
   data () {
     return {
-      title: ''
+      book: {
+        title: '',
+        author: '',
+        genre: '',
+        country: '',
+        synopsis: ''
+      }
     }
   },
   mounted () {
@@ -19,7 +42,7 @@ export default {
       .then(res => {
         if (res.data.success) {
           console.log(res.data)
-          this.title = res.data.selectedBook.title
+          this.book = res.data.selectedBook
         }
       })
       .catch(err => {
@@ -30,5 +53,44 @@ export default {
 </script>
 
 <style scoped>
+.personalised-suggestion{
+  display: flex;
+  justify-content: space-between;
+  width: 65vw;
+  padding: 2.5% 1.5% 3% 1.5%;
+  border-radius: 5px;
+  cursor: default;
+}
 
+.img{
+    margin: auto;
+    border-radius: 3px;
+    box-shadow: 0 8px 10px rgba(108, 106, 106, 0.25), 0 2px 7px rgba(188, 186, 186, 0.22);
+    position: relative !important;
+}
+
+.book-infos{
+  margin-left: 6%;
+  margin-top: 7%;
+}
+
+.synopsis-title{
+  font-weight: bold !important;
+  margin-top: 6%;
+}
+
+.btns{
+  display: flex;
+}
+button{
+  font-weight: bold;
+}
+
+/* RESPONSIVE --  RESPONSIVE -- RESPONSIVE -- RESPONSIVE -- RESPONSIVE -- */
+@media(max-width: 850px){
+  .btns{
+    flex-direction: column;
+    width: 90%;
+  }
+}
 </style>
