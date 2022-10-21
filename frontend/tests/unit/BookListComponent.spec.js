2472e56c71468c4fbf4e918374a73702dbec5653
lysiandon <don.lysiane@gmail.com>
2022-10-21 16:49:24 +0200

    merged user-library

---
+++ frontend/tests/unit/BookListComponent.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils'
+import BookListComponent from '@/components/admin/BookListComponent.vue'
+
+describe('BookListComponent.vue', () => {
+  it('renders the number of books in bookList when set in data', () => {
+    const wrapper = shallowMount(BookListComponent, {
+      computed: {
+        numberOfBooks () {
+            return 3;
+        }
+      }
+    })
+    const numberOfBooks = wrapper.find('h4').text()
+    expect(numberOfBooks).toMatch('3 résultat(s)')
+  })
+})
