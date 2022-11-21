f56ed18d7fa2c6c6383b324d06208d16e4d2d77a
lysiandon <don.lysiane@gmail.com>
2022-11-21 22:35:11 +0100

    fail :r:

--- frontend/tests/unit/BookListComponent.spec.js
+++ frontend/tests/unit/BookListComponent.spec.js
@@ -11,3 +11,17 @@ describe('BookListComponent.vue', () => {
     expect(numberOfBooks).toMatch('3 résultat(s)')
   })
 })
+
+// describe('BookListComponent.vue', () => {
+//   it('renders the number of books in bookList when set in data', () => {
+//     const wrapper = shallowMount(BookListComponent, {
+//       computed: {
+//         numberOfBooks () {
+//           return 3
+//         }
+//       }
+//     })
+//     const numberOfBooks = wrapper.find('h4').text()
+//     expect(numberOfBooks).toMatch('3 résultat(s)')
+//     })
+//   })
